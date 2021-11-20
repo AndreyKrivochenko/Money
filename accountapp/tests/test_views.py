6689fdbd201fa4f48cf1e91df4baf80f5f772920
Andrey Krivochenko <krivochenko_andrey@mail.ru>
2021-11-20 14:40:25 +0700

    Обновил тесты

--- accountapp/tests/test_views.py
+++ accountapp/tests/test_views.py
@@ -5,7 +5,17 @@ class AccountAppMainTemplateTest(TestCase):
     def setUp(self) -> None:
         self.client = Client()
 
-    def test_context(self):
+    def test_context_account(self):
         response = self.client.get('/account/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['title'], 'Счета')
+
+    def test_context_services(self):
+        response = self.client.get('/account/services/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['title'], 'Услуги')
+
+    def test_context_property(self):
+        response = self.client.get('/account/property/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['title'], 'Имущество')
